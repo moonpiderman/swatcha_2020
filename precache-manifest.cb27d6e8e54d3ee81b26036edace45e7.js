04c16c7d3001a2a42763bc3c7db2c5678eb2830f
moonpiderman <70942197+moonpiderman@users.noreply.github.com>
2020-10-08 00:31:37 +0900

    Updates

---
+++ precache-manifest.cb27d6e8e54d3ee81b26036edace45e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065420016eb2c9c581ca82915e6659fa",
+    "url": "/swatcha_2020/index.html"
+  },
+  {
+    "revision": "f1204a864a608a58771a",
+    "url": "/swatcha_2020/static/css/main.705b7061.chunk.css"
+  },
+  {
+    "revision": "7b83326e7488178dcede",
+    "url": "/swatcha_2020/static/js/2.e9281d00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swatcha_2020/static/js/2.e9281d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1204a864a608a58771a",
+    "url": "/swatcha_2020/static/js/main.11eda867.chunk.js"
+  },
+  {
+    "revision": "55def018294d18d78553",
+    "url": "/swatcha_2020/static/js/runtime-main.9c4eb696.js"
+  }
+]);
